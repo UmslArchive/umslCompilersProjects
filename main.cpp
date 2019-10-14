ae7a8f8b9f62cea495912f1dd01f389baf554695
Colby Ackerman <colby@thinkhappymedium.com>
2019-10-14 10:16:13 -0500

    main shelled

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//Author:   Colby Ackerman
+//Class:    CS4280 Program Translations
+//Assign:   Project 1
+//Date:     10/20/19
+//-----------------------------------------------------------------------------
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
