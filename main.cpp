c6622c598373c53aa1741e40ee9bcd4cad4e3e78
Colby Ackerman <colby@thinkhappymedium.com>
2019-10-25 12:47:21 -0500

    separated other tests from main scanner test

--- main.cpp
+++ main.cpp
@@ -6,10 +6,23 @@
 
 #include "TestScanner.h"
 
+//Change this to either run all tests or just the scanner test. [0, 1]
+#define RUN_ONLY_SCANNER_TESTS 0
+
 int main(int argc, char* argv[]) {
 
     TestScanner tester;
-    tester.runTokenTests();
+
+    if(RUN_ONLY_SCANNER_TESTS == 1) {
+        tester.testScanner();
+    }
+    else {
+        tester.runTokenTests();
+    }
+
+    
+
+    
 
     return 0;
 }
