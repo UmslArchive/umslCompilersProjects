68bb1c4623798afae8969c0fea862684bcb9c85c
Colby Ackerman <colby@thinkhappymedium.com>
2019-11-11 11:22:43 -0600

    increased column spacing for token printing

--- Token.cpp
+++ Token.cpp
@@ -64,7 +64,7 @@ std::string Token::idName(int id) {
 
 //Debug
 void Token::printToken() {
-    int columnSpacing = 27;
+    int columnSpacing = 35;
 
     std::stringstream ss;
     std::string idString;
