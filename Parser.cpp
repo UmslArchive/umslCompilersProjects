c469d701fbc7a48c4cf0f4ff050ad595d1afc4ba
Colby Ackerman <colby@thinkhappymedium.com>
2019-11-13 11:44:45 -0600

    shelled parser files

---
+++ Parser.cpp
@@ -0,0 +1,7 @@
+//Author:   Colby Ackerman
+//Class:    CS4280 Program Translations
+//Assign:   Project 1
+//Date:     10/20/19
+//-----------------------------------------------------------------------------
+
+#include "Parser.h"
---
+++ Parser.h
@@ -0,0 +1,20 @@
+//Author:   Colby Ackerman
+//Class:    CS4280 Program Translations
+//Assign:   Project 1
+//Date:     10/20/19
+//-----------------------------------------------------------------------------
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "Scanner.h"
+
+class Parser  {
+private:
+    Scanner scanner;
+
+public:
+
+};
+
+#endif
