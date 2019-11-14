a1b6e857d3f825eb8513f8c88bb19e1ab55d6693
Colby Ackerman <colby@thinkhappymedium.com>
2019-11-13 18:37:28 -0600

    added empty definitions for nonterminal functions

--- Parser.cpp
+++ Parser.cpp
@@ -6,10 +6,84 @@
 
 #include "Parser.h"
 
+//Constructor
 Parser::Parser() :
     currentToken(Token(ERROR_tk, "PARSE_BEGIN", 0))
 {}
 
+//Init
 void Parser::sendFileDataToScanner(std::string fileData) {
     scanner.loadFileData(fileData);
+}
+
+ParseTree Parser::parse() {
+    return ParseTree();
+}
+
+void Parser::program() {
+
+}
+
+void Parser::block() {
+
+}
+
+void Parser::vars() {
+
+}
+
+void Parser::expr() {
+
+}
+
+void Parser::A() {
+
+}
+
+void Parser::N() {
+
+}
+
+void Parser::M() {
+
+}
+
+void Parser::R() {
+
+}
+
+void Parser::stats() {
+
+}
+
+void Parser::mStat() {
+
+}
+
+void Parser::stat() {
+
+}
+
+void Parser::in() {
+
+}
+
+void Parser::out() {
+
+}
+
+void Parser::IF() {
+
+}
+
+void Parser::loop() {
+
+}
+
+void Parser::assign() {
+
+}
+
+void Parser::RO() {
+
 }
